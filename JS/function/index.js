21665d7223b1b4853a2f9360658e636b569db988
Your Name <y.y.oleksander@gmail.com>
2022-02-16 14:41:55 +0200

    220216

---
+++ JS/function/index.js
@@ -0,0 +1,23 @@
+function getSenseOfLif() {
+  return 42;
+}
+function sum(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+function getSquared(num) {
+  return num * num;
+}
+
+function sum(age) {
+  console.log('I am ' + age + ' old');
+}
+let age = 34;
+sum(age);
+
+const mult = (firstNum, secondNum) => firstNum * secondNum;
+
+const square = num => num * num;
+console.log(square(9));
+
+const getMagicNumber = () => 17;
